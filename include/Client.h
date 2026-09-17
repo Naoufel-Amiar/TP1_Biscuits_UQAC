986366ce19ce15d6dadde9dfa91c686d853f073e
Thomas <vidil@et.esiea.fr>
2026-09-17 21:13:28 +0200

    Mise en place de la liste Chainée pour les Clients
    J'ai préparer Tout ce qui servira aux liste chainées pour les clients

--- include/Client.h
+++ include/Client.h
@@ -4,14 +4,24 @@
 #include <string>
 #include "Commande.h"
 
+namespace std{
+
 struct Client
 {
-    std::string nom;
+    string nom;
     int numero;
-    std::string rue;
+    string Rue;
 
     Commande* premiereCommande;
     Client* suivant;
 };
+
+Client* NewClient (string Name, int Num, string Rue); //Création des maillons un a un, ne pas appeler
+
+void ADD (Client* Liste_Clients, string Name, int Num, string Rue); //Création de la Liste Chainée des Clients,
+
+void DelListClient (Client* Liste_Clients); //destruction de la Liste Chainée
+
+}
 
 #endif
--- source/Client.cpp
+++ source/Client.cpp
@@ -1,4 +1,48 @@
 #include "Client.h"
 
+namespace std{
+
+Client* NewClient (string Name, int Num, string Rue){
+    Client* Cl = new Client;
+    Cl->nom = Name;
+    Cl->numero = Num;
+    Cl->Rue = Rue;
+    Cl->premiereCommande = NULL;
+    Cl->suivant = NULL;
+    return Cl;
+}
+
+void ADD (Client* Liste_Clients, string Name, int Num, string Rue){
+    if (Liste_Clients == NULL){
+        Liste_Clients = NewClient(Name, Num, Rue);
+    }
+    Client* Ptr_Actuel;
+    Ptr_Actuel = Liste_Clients;
+    while (Ptr_Actuel->suivant != NULL){
+        Ptr_Actuel = Ptr_Actuel->suivant;
+    }
+    Ptr_Actuel->suivant = NewClient(Name, Num, Rue);
+}
+
+
+void DelListClient (Client* Liste_Clients){
+    Client* Actu;
+    Client* suivant = NULL;
+    Actu = Liste_Clients;
+    while (Actu != NULL){
+
+            suivant = Actu->suivant;
+            delete Actu;
+            Actu = suivant;
+    }
+}
+
+
+
+
+}
+
+
+
 // Ce fichier pourra contenir des fonctions liees aux clients
 // si le projet en a besoin par la suite.
